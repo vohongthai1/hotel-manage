9207169d1e7c80000e8281920616803da8093e38
vohongthai1 <vothai613@gmail.com>
2025-12-05 12:36:17 +0700

    Initial commit: Hotel Management System with C# and SQL Server

---
+++ HotelManager/DAO/DataProvider.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManager.DAO
+{
+    public class DataProvider
+    {
+        private static DataProvider instance;
+
+        // CHUỖI KẾT NỐI ĐÚNG THEO MÁY BẠN (TỪ SSMS)
+        private string connectionStr =
+            @"Data Source=VOHONGTHAI\SQLEXPRESS;Initial Catalog=HotelManagement;Integrated Security=True;TrustServerCertificate=True;";
+
+        // Các dòng cũ (đã comment để tránh nhầm)
+        // private string connectionStr = @"Data Source=DESKTOP-83IOFVC;Initial Catalog=Hotel_ManagentBK;Integrated Security=True";
+        // private string connectionStr = @"Data Source=THIEN-AI\THIENAI;Initial Catalog=HotelManagement;Integrated Security=True";
+        // private string connectionStr = @"Data Source=.\sqlexpress;Initial Catalog=HotelManagement;Integrated Security=True";
+
+        public DataTable ExecuteQuery(string query, object[] parameter = null)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    AddParameter(query, parameter, command);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(data);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Lỗi kết nối CSDL: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            return data;
+        }
+
+        public int ExecuteNoneQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    AddParameter(query, parameter, command);
+                    data = command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Lỗi thực thi lệnh: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            return data;
+        }
+
+        public object ExecuteScalar(string query, object[] parameter = null)
+        {
+            object data = null;
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    AddParameter(query, parameter, command);
+                    data = command.ExecuteScalar();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Lỗi thực thi Scalar: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            return data;
+        }
+
+        private void AddParameter(string query, object[] parameter, SqlCommand command)
+        {
+            if (parameter != null)
+            {
+                string[] listParameter = query.Split(' ');
+                int i = 0;
+                foreach (string item in listParameter)
+                {
+                    if (item.Contains("@") && i < parameter.Length)
+                    {
+                        command.Parameters.AddWithValue(item, parameter[i++]);
+                    }
+                }
+            }
+        }
+
+        // Singleton Pattern
+        public static DataProvider Instance
+        {
+            get { if (instance == null) instance = new DataProvider(); return instance; }
+            private set => instance = value;
+        }
+
+        private DataProvider() { }
+    }
+}
---
+++ HotelManager/fLogin.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace HotelManager
+{
+    partial class fLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fLogin));
+            this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txbUserName = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.txbPassWord = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnClose = new Bunifu.Framework.UI.BunifuImageButton();
+            this.btnLogin = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.bunifuSeparator1 = new Bunifu.Framework.UI.BunifuSeparator();
+            this.btnExit_ = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.bunifuSeparator2 = new Bunifu.Framework.UI.BunifuSeparator();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = this;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(488, 31);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(231, 55);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Đăng Nhập";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(492, 111);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tên đăng nhập:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(492, 226);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 31);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Mật khẩu:";
+            // 
+            // txbUserName
+            // 
+            this.txbUserName.BorderColorFocused = System.Drawing.Color.Black;
+            this.txbUserName.BorderColorIdle = System.Drawing.Color.Black;
+            this.txbUserName.BorderColorMouseHover = System.Drawing.Color.Black;
+            this.txbUserName.BorderThickness = 1;
+            this.txbUserName.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txbUserName.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txbUserName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txbUserName.isPassword = false;
+            this.txbUserName.Location = new System.Drawing.Point(498, 149);
+            this.txbUserName.Margin = new System.Windows.Forms.Padding(6);
+            this.txbUserName.Name = "txbUserName";
+            this.txbUserName.Size = new System.Drawing.Size(555, 61);
+            this.txbUserName.TabIndex = 0;
+            this.txbUserName.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // txbPassWord
+            // 
+            this.txbPassWord.BorderColorFocused = System.Drawing.Color.Black;
+            this.txbPassWord.BorderColorIdle = System.Drawing.Color.Black;
+            this.txbPassWord.BorderColorMouseHover = System.Drawing.Color.Black;
+            this.txbPassWord.BorderThickness = 1;
+            this.txbPassWord.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txbPassWord.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txbPassWord.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txbPassWord.isPassword = true;
+            this.txbPassWord.Location = new System.Drawing.Point(498, 263);
+            this.txbPassWord.Margin = new System.Windows.Forms.Padding(6);
+            this.txbPassWord.Name = "txbPassWord";
+            this.txbPassWord.Size = new System.Drawing.Size(555, 61);
+            this.txbPassWord.TabIndex = 1;
+            this.txbPassWord.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txbPassWord.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbPassWord_KeyPress);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(422, 545);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(55, 31);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(299, 168);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.SeaGreen;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(18, 234);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(386, 292);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Transparent;
+            this.btnClose.Image = ((System.Drawing.Image)(resources.GetObject("btnClose.Image")));
+            this.btnClose.ImageActive = null;
+            this.btnClose.Location = new System.Drawing.Point(1101, 8);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(24, 25);
+            this.btnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnClose.TabIndex = 19;
+            this.btnClose.TabStop = false;
+            this.btnClose.Zoom = 10;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.ActiveBorderThickness = 1;
+            this.btnLogin.ActiveCornerRadius = 20;
+            this.btnLogin.ActiveFillColor = System.Drawing.Color.Black;
+            this.btnLogin.ActiveForecolor = System.Drawing.Color.White;
+            this.btnLogin.ActiveLineColor = System.Drawing.Color.Black;
+            this.btnLogin.BackColor = System.Drawing.Color.White;
+            this.btnLogin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnLogin.BackgroundImage")));
+            this.btnLogin.ButtonText = "Đăng Nhập";
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.Black;
+            this.btnLogin.IdleBorderThickness = 1;
+            this.btnLogin.IdleCornerRadius = 20;
+            this.btnLogin.IdleFillColor = System.Drawing.Color.White;
+            this.btnLogin.IdleForecolor = System.Drawing.Color.Black;
+            this.btnLogin.IdleLineColor = System.Drawing.Color.Black;
+            this.btnLogin.Location = new System.Drawing.Point(498, 369);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(555, 62);
+            this.btnLogin.TabIndex = 22;
+            this.btnLogin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click_1);
+            // 
+            // bunifuSeparator1
+            // 
+            this.bunifuSeparator1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuSeparator1.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.bunifuSeparator1.LineThickness = 1;
+            this.bunifuSeparator1.Location = new System.Drawing.Point(498, 89);
+            this.bunifuSeparator1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.bunifuSeparator1.Name = "bunifuSeparator1";
+            this.bunifuSeparator1.Size = new System.Drawing.Size(555, 15);
+            this.bunifuSeparator1.TabIndex = 28;
+            this.bunifuSeparator1.Transparency = 255;
+            this.bunifuSeparator1.Vertical = false;
+            // 
+            // btnExit_
+            // 
+            this.btnExit_.ActiveBorderThickness = 1;
+            this.btnExit_.ActiveCornerRadius = 20;
+            this.btnExit_.ActiveFillColor = System.Drawing.Color.Black;
+            this.btnExit_.ActiveForecolor = System.Drawing.Color.White;
+            this.btnExit_.ActiveLineColor = System.Drawing.Color.Black;
+            this.btnExit_.BackColor = System.Drawing.Color.White;
+            this.btnExit_.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnExit_.BackgroundImage")));
+            this.btnExit_.ButtonText = "Thoát";
+            this.btnExit_.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit_.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit_.ForeColor = System.Drawing.Color.Black;
+            this.btnExit_.IdleBorderThickness = 1;
+            this.btnExit_.IdleCornerRadius = 20;
+            this.btnExit_.IdleFillColor = System.Drawing.Color.White;
+            this.btnExit_.IdleForecolor = System.Drawing.Color.Black;
+            this.btnExit_.IdleLineColor = System.Drawing.Color.Black;
+            this.btnExit_.Location = new System.Drawing.Point(498, 452);
+            this.btnExit_.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.btnExit_.Name = "btnExit_";
+            this.btnExit_.Size = new System.Drawing.Size(555, 62);
+            this.btnExit_.TabIndex = 29;
+            this.btnExit_.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnExit_.Click += new System.EventHandler(this.btnExit__Click);
+            // 
+            // bunifuSeparator2
+            // 
+            this.bunifuSeparator2.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuSeparator2.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.bunifuSeparator2.LineThickness = 1;
+            this.bunifuSeparator2.Location = new System.Drawing.Point(498, 338);
+            this.bunifuSeparator2.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.bunifuSeparator2.Name = "bunifuSeparator2";
+            this.bunifuSeparator2.Size = new System.Drawing.Size(555, 15);
+            this.bunifuSeparator2.TabIndex = 30;
+            this.bunifuSeparator2.Transparency = 255;
+            this.bunifuSeparator2.Vertical = false;
+            // 
+            // fLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1132, 545);
+            this.Controls.Add(this.bunifuSeparator2);
+            this.Controls.Add(this.btnExit_);
+            this.Controls.Add(this.bunifuSeparator1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txbPassWord);
+            this.Controls.Add(this.txbUserName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "fLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Bunifu.Framework.UI.BunifuDragControl bunifuDragControl1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private Bunifu.Framework.UI.BunifuMetroTextbox txbUserName;
+        private Bunifu.Framework.UI.BunifuMetroTextbox txbPassWord;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private Bunifu.Framework.UI.BunifuImageButton btnClose;
+        private Bunifu.Framework.UI.BunifuThinButton2 btnLogin;
+        private Bunifu.Framework.UI.BunifuSeparator bunifuSeparator1;
+        private Bunifu.Framework.UI.BunifuThinButton2 btnExit_;
+        private Bunifu.Framework.UI.BunifuSeparator bunifuSeparator2;
+    }
+}
+
